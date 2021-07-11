f2b83d2da38f4f048a818ba76941a3bcb4668060
kenzzz <35756289+Kens14@users.noreply.github.com>
2021-07-11 19:12:49 +0530

    ver2

--- level1/stage 10/script.js
+++
@@ -1,9 +0,0 @@
-
-        var element = document.querySelector("clue");
-
-     $("#key").click(function () {
-
-        element.style.animation = 'shake 3s cubic-bezier(.36,.07,.19,.97) both';
-     });
-  
- 
